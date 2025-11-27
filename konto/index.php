48e67f89b7935929b88cc9218903586e9b47104e
Emilsen <79190987+Emilsen2152@users.noreply.github.com>
2025-11-27 13:09:49 +0100

    Add Konto page and authentication system

---
+++ konto/index.php
@@ -0,0 +1,37 @@
+<?php
+include 'system/authenticate.php';
+
+$user = getUser($API_BASE);
+
+if (!$user) {
+    header('Location: login.php');
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="no">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Konto - EmilWEB</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</head>
+
+<body class="bg-dark">
+    <?php
+    include 'includes/navbar.php';
+    ?>
+
+    <div class="container-fluid bg-light py-5">
+        
+    </div>
+
+    <?php
+    include 'includes/footer.php';
+    ?>
+</body>
+
+</html>
