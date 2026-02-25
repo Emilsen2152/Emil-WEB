9b969937804331abcb335218008ae09a7b002cf6
Emilsen <79190987+Emilsen2152@users.noreply.github.com>
2026-02-25 15:29:30 +0100

    Add functions to retrieve user and shared to-do lists

--- oppgaver/vg1/d01t/bootstrap.php
+++ oppgaver/vg1/d01t/bootstrap.php
@@ -3,5 +3,29 @@ declare(strict_types=1);
 
 require_once __DIR__ . '../../../../api/bootstrap.php';
 
-echo $pdo;
-?>
+function get_user_to_do_lists(PDO $pdo, array $config): array
+{
+    $user = current_user($pdo, $config);
+    if (!$user) {
+        return [];
+    }
+
+    $stmt = $pdo->prepare('SELECT * FROM to_do_lists WHERE owner_id = ?');
+    $stmt->execute([$user['id']]);
+    return $stmt->fetchAll();
+}
+
+function get_shared_to_do_lists(PDO $pdo, array $config): array
+{
+    $user = current_user($pdo, $config);
+    if (!$user) return [];
+
+    $stmt = $pdo->prepare('
+        SELECT l.*
+        FROM to_do_lists l
+        INNER JOIN shared_to_do_lists s ON s.table_id = l.id
+        WHERE s.user_id = ?
+    ');
+    $stmt->execute([$user['id']]);
+    return $stmt->fetchAll();
+}
