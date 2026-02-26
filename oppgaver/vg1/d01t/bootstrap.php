8fd43e29b83f52b08a21f9ea257ada5844eeb50d
Emilsen <79190987+Emilsen2152@users.noreply.github.com>
2026-02-26 09:11:33 +0100

    Add functions to retrieve to-do list items and create new to-do lists

--- oppgaver/vg1/d01t/bootstrap.php
+++ oppgaver/vg1/d01t/bootstrap.php
@@ -29,3 +29,20 @@ function get_shared_to_do_lists(PDO $pdo, array $config): array
     $stmt->execute([$user['id']]);
     return $stmt->fetchAll();
 }
+
+function get_to_do_list_items(PDO $pdo, int $to_do_list_id): array
+{
+    $stmt = $pdo->prepare('SELECT * FROM to_do_items WHERE to_do_list_id = ?');
+    $stmt->execute([$to_do_list_id]);
+    return $stmt->fetchAll();
+}
+
+function create_to_do_list(PDO $pdo, array $config, string $name): ?array
+{
+    $user = current_user($pdo, $config);
+    if (!$user) return null;
+
+    $stmt = $pdo->prepare('INSERT INTO to_do_lists (name, owner_id) VALUES (?, ?)');
+    $stmt->execute([$name, $user['id']]);
+    return ['id' => (int)$pdo->lastInsertId(), 'name' => $name];
+}
